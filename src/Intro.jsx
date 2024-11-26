95236eae42424f8309b066b4f0505d09c211dcc6
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-11-26 15:54:49 -0800

    Create Intro section + components

---
+++ src/Intro.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>The Physiological Effects of Alert Systems</h1>
+      <p>
+        Emergency alert systems play a critical role in public safety, providing
+        timely warnings during disasters and emergencies. Two prominent systems
+        in the United States are the Emergency Alert System (EAS) and Wireless
+        Emergency Alerts (WEAs). While both systems aim to save lives, their
+        design and delivery impact their effectiveness and the physiological
+        stress responses they elicit. This paper examines how these systems
+        function, explores the physiological effects of emergency alerts, and
+        highlights the advantages of WEAs over EAS, while addressing gaps in
+        accessibility and equity.
+      </p>
+    </>
+  );
+}
+
+export default App;
